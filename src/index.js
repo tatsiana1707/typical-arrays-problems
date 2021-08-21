129dbdfca23ac3df81c78cfc754e3aff1fcb088a
Tatsiana1707 <tannysha1987@mail.ru>
2021-08-21 10:13:39 +0300

    feat: add typical-arrays-problems solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
 exports.min = function min (array) {
-  return 0;
+    
+    if ( !Array.isArray(array) || (array.length == 0) ) {
+    return 0;
+} else {
+    return Math.min.apply(null, array);
 }
 
+}
+
+
 exports.max = function max (array) {
-  return 0;
+    if ( !Array.isArray(array) || (array.length == 0) ) {
+        return 0;
+    } else {
+        return Math.max.apply(null, array);
+    }
+    
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if(!Array.isArray(array) || array.length === 0) {
+        return 0;
+    } else{
+        return array.reduce((a,b)=>a+b)/array.length;
+    }
 }
